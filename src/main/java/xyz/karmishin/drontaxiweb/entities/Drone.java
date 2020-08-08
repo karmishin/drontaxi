b9564499e75041b143823d55059ac044d16fc9a2
Mikhail Karmishin <20360770+karmishin@users.noreply.github.com>
2020-08-08 17:41:24 +0300

    remove lombok

--- src/main/java/xyz/karmishin/drontaxiweb/entities/Drone.java
+++ src/main/java/xyz/karmishin/drontaxiweb/entities/Drone.java
@@ -1,19 +1,66 @@
 package xyz.karmishin.drontaxiweb.entities;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Data
 @Entity
 public class Drone {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String manufacturer, model, licensePlate;
     private LocalDateTime productionDate, registrationDate, writeOffDate;
-    @OneToMany(mappedBy = "drone")
-    private List<Order> orders;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public LocalDateTime getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(LocalDateTime productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDateTime getWriteOffDate() {
+        return writeOffDate;
+    }
+
+    public void setWriteOffDate(LocalDateTime writeOffDate) {
+        this.writeOffDate = writeOffDate;
+    }
 }
--- src/main/java/xyz/karmishin/drontaxiweb/entities/Order.java
+++ src/main/java/xyz/karmishin/drontaxiweb/entities/Order.java
@@ -1,26 +1,94 @@
 package xyz.karmishin.drontaxiweb.entities;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Data
 @Entity
 @Table(name = "t_order")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private LocalDateTime date;
+
     @ManyToOne
     private User user;
+
     @ManyToOne
     private User operator;
+
     private String departureAddress, destinationAddress;
-    private enum Status { EN_ROUTE, WAITING, COMPLETE, CANCELLED };
+
+    private enum Status {
+        EN_ROUTE,
+        WAITING,
+        COMPLETE,
+        CANCELLED
+    }
+
     private Status currentStatus;
+
     @ManyToOne
-    @JoinColumn(name = "drone_id")
     private Drone drone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getOperator() {
+        return operator;
+    }
+
+    public void setOperator(User operator) {
+        this.operator = operator;
+    }
+
+    public String getDepartureAddress() {
+        return departureAddress;
+    }
+
+    public void setDepartureAddress(String departureAddress) {
+        this.departureAddress = departureAddress;
+    }
+
+    public String getDestinationAddress() {
+        return destinationAddress;
+    }
+
+    public void setDestinationAddress(String destinationAddress) {
+        this.destinationAddress = destinationAddress;
+    }
+
+    public Status getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void setCurrentStatus(Status currentStatus) {
+        this.currentStatus = currentStatus;
+    }
+
+    public Drone getDrone() {
+        return drone;
+    }
+
+    public void setDrone(Drone drone) {
+        this.drone = drone;
+    }
 }
